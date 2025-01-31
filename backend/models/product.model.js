ad037543bbb3abdee0e52ea8f87c1972abbc49a8
Prashant Mishra <prashantmishra10232@gmail.com>
2025-01-31 14:14:27 +0530

    added the review section to the product model

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,43 +1,72 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please Enter product Name"],
-        trim: true,
-      },
-      description: {
-        type: String,
-        required: [true, "Please Enter product Description"],
-      },
-      price: {
-        type: Number,
-        required: [true, "Please Enter product Price"],
-        maxLength: [8, "Price cannot exceed 8 characters"],
-      },
-      ratings: {
-        type: Number,
-        default: 0,
+      type: String,
+      required: [true, "Please Enter product Name"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please Enter product Description"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please Enter product Price"],
+      maxLength: [8, "Price cannot exceed 8 characters"],
+    },
+    ratings: {
+      type: Number,
+      default: 0,
     },
 
     images: [
-        {
-          public_id: {
-            type: String,
-            required: true,
-          },
-          url: {
-            type: String,
-            required: true,
-          },
+      {
+        public_id: {
+          type: String,
+          required: true,
         },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
     ],
 
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    }
-},{timestamps:true});
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Product = mongoose.model("Product",productSchema)
+export const Product = mongoose.model("Product", productSchema);
