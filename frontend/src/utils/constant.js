41053b2c99c888f9849b9a472f766d51c9c89328
Prashant Mishra <prashantmishra10232@gmail.com>
2025-07-13 12:58:30 +0530

    render settings

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,12 @@
-export const USER_API_ENDPOINT="http://localhost:5000/api/v1/user"
+// local
 
-export const Product_API_ENDPOINT="http://localhost:5000/api/v1/product"
+// export const USER_API_ENDPOINT="http://localhost:5000/api/v1/user"
+
+// export const Product_API_ENDPOINT="http://localhost:5000/api/v1/product"
+
+
+//for deployment
+
+export const USER_API_ENDPOINT="https://rarekraft-in.onrender.com/user"
+
+export const Product_API_ENDPOINT="https://rarekraft-in.onrender.com/product"
