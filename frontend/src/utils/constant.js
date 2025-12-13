280277609e864158160a7aed5dace67d1d343136
PrashantNeolytix10232 <prashant@neolytix.com>
2025-12-13 21:47:04 +0530

    constant

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,23 +1,23 @@
 // local
 
-export const USER_API_ENDPOINT="http://localhost:5000/api/v1/user"
+// export const USER_API_ENDPOINT="http://localhost:5000/api/v1/user"
 
-export const Product_API_ENDPOINT="http://localhost:5000/api/v1/product"
+// export const Product_API_ENDPOINT="http://localhost:5000/api/v1/product"
 
-export const CART_API_ENDPOINT="http://localhost:5000/api/v1/cart"
+// export const CART_API_ENDPOINT="http://localhost:5000/api/v1/cart"
 
-export const ORDER_API_ENDPOINT="http://localhost:5000/api/v1/order"
+// export const ORDER_API_ENDPOINT="http://localhost:5000/api/v1/order"
 
-export const PAYMENT_API_ENDPOINT="http://localhost:5000/api/v1/payment"
+// export const PAYMENT_API_ENDPOINT="http://localhost:5000/api/v1/payment"
 
 //for deployment
 
-// export const USER_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/user"
+export const USER_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/user"
 
-// export const Product_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/product"
+export const Product_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/product"
 
-// export const CART_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/cart"
+export const CART_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/cart"
 
-// export const ORDER_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/order"
+export const ORDER_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/order"
 
-// export const PAYMENT_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/payment"
+export const PAYMENT_API_ENDPOINT="https://rarekraft-in.onrender.com/api/v1/payment"
